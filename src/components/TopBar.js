797fc74389413ac13303d3fb26c9729b65868797
“felix” <felixho1993@gmail.com>
2019-06-05 14:41:40 +0800

    Fix

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Container, Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
+import {Nav, Navbar, NavDropdown, Form, FormControl, Button} from 'react-bootstrap';
 
 class TopBar extends React.Component {
   constructor(props) {
@@ -20,21 +20,27 @@ class TopBar extends React.Component {
 
   render() {
     return (
-      <Navbar color="dark" dark expand="md">
-        <Container>
-          <NavbarBrand href="/">
-            <span>{this.state.name}</span>
-          </NavbarBrand>
-          <NavbarToggler onClick={this.toggle}/>
-          <Collapse isOpen={this.state.isOpen} navbar>
-            <Nav className="ml-auto" navbar>
-              <NavItem>
-                <NavLink href="/profile/">Profile</NavLink>
-              </NavItem>
-            </Nav>
-          </Collapse>
-        </Container>
-      </Navbar>
+    <Navbar bg="light" expand="lg">
+      <Navbar.Brand href="#home">{this.state.name}</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mr-auto">
+          <Nav.Link href="#home">Home</Nav.Link>
+          <Nav.Link href="#link">Link</Nav.Link>
+          <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+            <NavDropdown.Divider />
+            <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+          </NavDropdown>
+        </Nav>
+        <Form inline>
+          <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+          <Button variant="outline-success">Search</Button>
+        </Form>
+      </Navbar.Collapse>
+    </Navbar>
     );
   }
 }
