d71d6b71caea2fc0c53cf1c3bc4b20f8ec7a5a56
Man Hon Ho <felix@Mans-MacBook-Pro.local>
2020-09-04 14:57:16 +0800

    Updates

--- precache-manifest.facac327ce689f3221fa96ebd6ae15a7.js
+++ precache-manifest.facac327ce689f3221fa96ebd6ae15a7.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "71aaf6bcc65424fe46661680ccdf9696",
+    "revision": "3c65ca58aec755ebd1e8d0367316441a",
     "url": "/myProfile/index.html"
   },
   {
-    "revision": "520c94dfa8b31d369325",
+    "revision": "e70179c16309a7c20eef",
     "url": "/myProfile/static/css/main.7d6cc20a.chunk.css"
   },
   {
-    "revision": "8d8cc607139dc27c6747",
-    "url": "/myProfile/static/js/2.b7d07c58.chunk.js"
+    "revision": "0402f2e97dd385556f80",
+    "url": "/myProfile/static/js/2.9d5c56c6.chunk.js"
   },
   {
-    "revision": "520c94dfa8b31d369325",
-    "url": "/myProfile/static/js/main.f4eefacc.chunk.js"
+    "revision": "e70179c16309a7c20eef",
+    "url": "/myProfile/static/js/main.10b48741.chunk.js"
   },
   {
     "revision": "ae570b6cd2be029927d5",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/myProfile/precache-manifest.d3ae94468f6c709bdd86c29b383ee707.js"
+  "/myProfile/precache-manifest.facac327ce689f3221fa96ebd6ae15a7.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/myProfile/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
