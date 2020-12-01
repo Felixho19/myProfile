e307c7236f0eb32717ae0327da71c1a636b04f06
Felixho19 <felixho1993@gmail.com>
2020-12-02 00:27:57 +0800

    Fix text

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -5,8 +5,10 @@ export default function Banner(){
     return (
     <Jumbotron>
         <Container>
-            <h1 className="display-3">Felix Ho</h1>
-            <p className="lead">I am a software developer from Hong Kong</p>
+            <h1 className="banner display-3">Felix Ho</h1>
+            <div className="mask">
+                <p className="desc">I am a software developer from Hong Kong</p>
+            </div>
         </Container>
     </Jumbotron>
     );
